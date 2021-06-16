1908546ce0973326bad97e3e418e79902948c819
mykha <efimenko.mykhailo@gmail.com>
2021-06-16 17:02:31 +0300

    Reduction first column to one

--- approx.rb
+++ approx.rb
@@ -5,7 +5,7 @@ puts m.inspect
 #byebug
 fk = m[0, 0]
 m.each_with_index do|el, row, col|
-  puts "#{el} at [#{row}][#{col}]"
+  #puts "#{el} at [#{row}][#{col}]"
   #byebug
   if col == 0
     fk = 1/el
@@ -13,3 +13,13 @@ m.each_with_index do|el, row, col|
   m[row,col] = el * fk
 end
 puts m.inspect
+
+m.each_with_index do|el, row, col|
+  #byebug
+  if row>0
+    m[row,col]=m[row,col]-m[0,col]
+  end 
+end
+
+puts m.inspect
+
