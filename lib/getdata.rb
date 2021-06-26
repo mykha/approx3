f11934af182f5d5e1075473336f04358cf687dfa
mykha <efimenko.mykhailo@gmail.com>
2021-06-26 16:53:49 +0300

    Create equation from data 0 step

---
+++ lib/getdata.rb
@@ -0,0 +1,19 @@
+def data_from_file
+  m = Dir.entries "./public"
+  data = []
+  m.each do |fname|
+    if fname[-4..fname.size] == ".txt"
+      input = File.open "./public/"+fname, "r"
+      while (line = input.gets)
+        coup = line.split " "
+        if coup[0]
+          data << { :x => coup[0].to_f, :y => coup[1].to_f }
+        end
+      end
+      # to skip nil values
+      data.select! {|item| item[:x] != nil }
+      #puts data.inspect
+      return data
+    end
+  end
+end
